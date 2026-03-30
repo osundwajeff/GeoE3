ec83aed296adf8af870a7611ec261f5608964af5
Tim Sutton <tim@kartoza.com>
2026-03-30 13:07:36 +0100

    Update QGIS start scripts to use GEOE3 profile (#313)
    * Update QGIS start scripts to use GEOE3 profile
    
    - Change start_qgis.sh from GEEST2 to GEOE3 profile
    - Add --profile GEOE3 to start_qgis_ltr.sh
    
    * Also launch QGIS under x11 rather than wayland protocol
    
    * Update admin to symlink to GEOE3 dir

--- scripts/start_qgis_ltr.sh
+++ scripts/start_qgis_ltr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "🪛 Running QGIS with the default profile:"
+echo "🪛 Running QGIS with the GEOE3 profile:"
 echo "--------------------------------"
 
 # Set environment variables (both GEOE3_* and GEEST_* for backward compatibility)
@@ -8,9 +8,11 @@ GEEST_TEST_DIR="$(pwd)/test" # Set test directory relative to project root
 
 # This is the flake approach, using Ivan Mincis nix spatial project and a flake
 # see flake.nix for implementation details
+# QT_QPA_PLATFORM flag forces it to run under x11 protocol
 GEOE3_LOG=${GEOE3_LOG} \
-    GEEST_LOG=${GEEST_LOG} \
-    GEOE3_TEST_DIR=${GEOE3_TEST_DIR} \
-    GEEST_TEST_DIR=${GEEST_TEST_DIR} \
-    RUNNING_ON_LOCAL=1 \
-    nix run .#qgis-ltr
+  GEEST_LOG=${GEEST_LOG} \
+  GEOE3_TEST_DIR=${GEOE3_TEST_DIR} \
+  GEEST_TEST_DIR=${GEEST_TEST_DIR} \
+  RUNNING_ON_LOCAL=1 \
+  QT_QPA_PLATFORM=xcb \
+  nix run .#qgis-ltr -- --profile GEOE3
